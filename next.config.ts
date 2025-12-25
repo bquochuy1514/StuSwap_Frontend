88ca0fe6dc4758580429211d768e91a0fbc040ef
bquochuy1514 <23520594@gm.uit.edu.vn>
2025-12-25 07:47:57 +0700

    thêm ignoreDuringBuilds

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+	eslint: {
+		ignoreDuringBuilds: true, // ← Thêm dòng này
+	},
 	images: {
 		domains: [
 			'localhost',
